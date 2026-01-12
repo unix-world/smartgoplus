85912f619a4598e07556396ccae54795d75c220e
Radu Ovidiu I. <ilies.ovidiu@googlemail.com>
2026-01-12 03:34:51 +0200

    Stable, svn.3103

--- smartgoplus.go
+++ smartgoplus.go
@@ -1,7 +1,7 @@
 
 // GO Lang :: SmartGo Plus :: Smart.Go.Framework
 // (c) 2021-present unix-world.org
-// r.20251229.2358 :: STABLE
+// r.20260111.2358 :: STABLE
 
 // REQUIRE: go 1.22 or later
 package smartgoplus
@@ -14,7 +14,7 @@ import (
 //-----
 
 const (
-	VERSION string = "v.20251229.2358"
+	VERSION string = "v.20260111.2358"
 	NAME string = "SmartGoPlus"
 )
 
